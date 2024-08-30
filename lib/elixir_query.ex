58ae66b96484d4e2c64d21bbde19b667cbc10012
Scai <59282365+alexevladgabriel@users.noreply.github.com>
2024-08-30 21:50:43 +0300

    base lib elixir

---
+++ lib/elixir_query.ex
@@ -0,0 +1,18 @@
+defmodule ElixirQuery do
+  @moduledoc """
+  Documentation for `ElixirQuery`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirQuery.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_query_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirQueryTest do
+  use ExUnit.Case
+  doctest ElixirQuery
+
+  test "greets the world" do
+    assert ElixirQuery.hello() == :world
+  end
+end
