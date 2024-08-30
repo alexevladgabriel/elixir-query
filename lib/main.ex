656255ed441d7a402c4b9cf87c2de2ad03470dff
Scai <59282365+alexevladgabriel@users.noreply.github.com>
2024-08-30 23:49:19 +0300

    feat: basic a2s query

--- lib/main.ex
+++ lib/main.ex
@@ -1,23 +1,22 @@
 defmodule ExQuery do
-  @moduledoc """
-  Documentation for `ElixirQuery`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirQuery.hello()
-      :world
-
-  """
   @spec query(
           hostname :: String.t(),
           port :: :inet.port_number()
-        ) :: any()
+        ) ::
+          {
+            :ok,
+            {A2S.Info.t(), A2S.Players.t()}
+          }
+          | {:error, :query_timeout}
+          | {:error, term()}
   def query(hostname, port) do
-    # check if the hostname is not a inet.ipv4_address
-    ExQuery.Dns.resolve(hostname)
+    with {:ok, ip} <- ExQuery.Dns.resolve(hostname),
+         {:info, info} <- A2S.Client.query(:info, {ip, port}),
+         {:players, players} <- A2S.Client.query(:players, {ip, port}) do
+      {:ok, {info, players}}
+    else
+      {:error, :recv_timeout} -> {:error, :query_timeout}
+      {:error, err} -> {:error, err}
+    end
   end
 end
